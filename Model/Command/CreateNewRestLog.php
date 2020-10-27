96c379b6eccc21d8ed81bf2eaad9cf4a64e41e41
Mateusz Niziołek <mateusz.niziolek@creativestyle.pl>
2020-10-27 23:12:22 +0100

    Refactor logging plugin into two separate plugins inovking command creating log object in each

---
+++ Model/Command/CreateNewRestLog.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace MageSuite\RestApiLogger\Model\Command;
+
+class CreateNewRestLog
+{
+    /**
+     * @var \MageSuite\RestApiLogger\Api\RestLogRepositoryInterface
+     */
+    protected $restLogRepository;
+
+    /**
+     * @var null | \MageSuite\RestApiLogger\Api\Data\RestLogInterface
+     */
+    protected $restLog;
+
+    public function __construct(\MageSuite\RestApiLogger\Api\RestLogRepositoryInterface $restLogRepository)
+    {
+        $this->restLogRepository = $restLogRepository;
+    }
+
+    public function execute($dataObject)
+    {
+        if($dataObject instanceof \Magento\Framework\App\RequestInterface) {
+            $this->restLog = $this->restLogRepository->create();
+            $this->restLog->setEndpoint($dataObject->getPathInfo());
+            $this->restLog->setPayload($dataObject->getContent());
+        }
+
+        if($dataObject instanceof  \Magento\Framework\Webapi\Rest\Response) {
+            $this->restLog->setResponseCode($dataObject->getStatusCode());
+            $this->restLog->setResponse($dataObject->getContent());
+        }
+
+        $this->restLogRepository->save($this->restLog);
+    }
+}
